7beb1765ac8e79aada52d3cdc215986befe7a4c4
sorooshhm <hamedifarsoroosh@gmail.com>
2024-12-28 11:15:53 +0330

    adding doors

--- game.c
+++ game.c
@@ -31,26 +31,37 @@ extern int maxY, maxX;
 
 void createRoom(Room *room, int rangeX, int rangeY)
 {
-    int width = (rand() % 4) + 4;
-    int height = (rand() % 4) + 4;
-    int x = (rand() % 48) + rangeX - width;
-    int y = (rand() % 17) + rangeY - height;
-    if (x < 0)
+    int width = (rand() % 6) + 6;
+    int height = (rand() % 6) + 4;
+    int x = (rand() % 49) + rangeX - width;
+    int y = (rand() % 18) + rangeY - height;
+    if (x <= 0)
     {
-        x = 0;
+        x = 2;
     }
-    if (x > 144)
+    if (x + width >= 144)
     {
         x = 85;
     }
-    if (y < 4)
+    if (y <= 4)
     {
-        y = 5;
+        y = 6;
     }
-    if (y > 40)
+    if (y + height >= 40)
     {
         y = 26;
     }
+    room->doors = (Door**)malloc(2*sizeof(Door*));
+    for (int i = 0; i < 2; i++)
+    {
+        room->doors[i] = (Door*)malloc(sizeof(Door));
+    }
+    
+    room->doors[0]->x = rand()%(width-1) + x;
+    room->doors[1]->x = rand()%(width-1) + x;
+    room->doors[0]->y = y;
+    room->doors[1]->y = y + height -1;
+    
     room->startX = x;
     room->startY = y;
     room->height = height;
@@ -61,38 +72,49 @@ void startGame(User *user, Mix_Music *music)
 {
     srand(time(NULL));
     printf("%d , %d\n", maxX, maxY);
+    noecho();
 
     Room **rooms = (Room **)malloc(6 * sizeof(Room *));
-    int rangeX = 0, rangeY = 4;
+    int rangeX = 3, rangeY = 4;
     for (int i = 0; i < 6; i++)
     {
         rooms[i] = (Room *)malloc(sizeof(Room));
-        createRoom(rooms[i], (i % 3) * 48 + rangeX, (i % 2) * 17 + rangeY);
+        createRoom(rooms[i], (i % 3) * 49 + rangeX, (i % 2) * 18 + rangeY);
         int x, y, width, height;
         x = rooms[i]->startX;
         y = rooms[i]->startY;
         height = rooms[i]->height;
         width = rooms[i]->width;
-        // printf("x %d y %d height %d width %d\n", x, y, height, width);
+        // printw("x %d y %d height %d width %d ", x, y, height, width);
 
         for (int j = x; j < x + width; j++)
         {
-            mvprintw(y, j, "_");
-            mvprintw(y + height - 1, j, "_");
+            mvprintw(y, j, "--");
+            mvprintw(y + height - 1, j, "--");
+            refresh();
         }
         for (int j = y + 1; j < y + height; j++)
         {
             mvprintw(j, x - 1, "|");
             mvprintw(j, x + width, "|");
+            refresh();
+
             for (int k = x + 1; k < x + width - 1; k++)
             {
-                if(j==y+height-1){
+                if (j == y + height - 1)
+                {
                     continue;
                 }
-                mvprintw(j, k, ".");
+                mvprintw(j, k, ":");
             }
+            refresh();
+        }
+        for (int j = 0; j < 2; j++)
+        {
+            mvprintw(rooms[i]->doors[j]->y,rooms[i]->doors[j]->x , "+");
         }
         refresh();
+        
     }
     while (1)
     {
